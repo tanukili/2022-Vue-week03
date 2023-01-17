9e15a5b86954725e07586562b515b219ddef4840
tanukili <108317269+tanukili@users.noreply.github.com>
2023-01-18 02:21:44 +0800

    取得產品資料

--- products.js
+++ products.js
@@ -4,8 +4,33 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue
 const app = {
     data() {
         return {
-            text: 1
+            // api 路徑
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            apiPath: 'vuejslive2022',
+            products: {},
+
         }
+    },
+    methods: {
+        // 串接 api ：取得產品資料
+        getData() {
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`
+            axios.get(url)
+                .then((res) => {
+                    this.products= res.data.products;
+                    console.log(this.products);
+                })
+                .catch((err) => {
+                    alert(err.data.message);
+                })
+        },
+    },
+    created() {
+        // 取出 token
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+        // 記得要加 this
+        this.getData();
     },
 };
 
