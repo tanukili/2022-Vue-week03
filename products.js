dc246dadc3c1b93c76534fdc6631c5b7367026ea
tanukili <108317269+tanukili@users.noreply.github.com>
2023-01-18 23:35:20 +0800

    modal 開啟

--- products.js
+++ products.js
@@ -1,6 +1,11 @@
 // 載入 Vue
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.min.js';
 
+// 宣告 Modal (因為全域會使用到)
+let productModal = {};
+
+let delProductModal = {};
+
 const app = {
     data() {
         return {
@@ -10,6 +15,22 @@ const app = {
             products: [],
 
         }
+    },
+    // 使用 created() 會報錯，因為是 html 渲染前調用；mounted() 則是渲染後再針對 html 的 dom 進行操作
+    mounted() {
+        // modal 初始化
+        productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+            keyboard: false
+        });
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            keyboard: false
+        });
+
+        // 取出 token
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+        // 記得要加 this
+        this.checkAdmin();
     },
     methods: {
         // 串接 api：檢查權限
@@ -23,7 +44,7 @@ const app = {
                 .catch((err) => {
                     alert(err.data.message);
                     // 導回登入頁面
-                    window.location = 'login.html'
+                    window.location = 'login.html';
                 })
         },
         // 串接 api：取得產品資料
@@ -37,14 +58,17 @@ const app = {
                     alert(err.data.message);
                 })
         },
+        // 開啟互動視窗
+        openModal(editStatus) {
+            if(editStatus === 'new'){
+                productModal.show();
+            }else if(editStatus === 'edit'){
+                productModal.show();
+            }else if(editStatus === 'delete'){
+                delProductModal.show();
+            }
+        }
         
-    },
-    created() {
-        // 取出 token
-        const token = document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-        axios.defaults.headers.common['Authorization'] = token;
-        // 記得要加 this
-        this.checkAdmin();
     },
 };
 
